f79435e88375ecb7ecb98128650cdec9c9c154b1
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2023-02-09 22:28:34 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/fed22d-grafiska-verktyg-tomatpureerna/',
+  base: '/school-project-graphic-design-wheatly/',
   plugins: [vue()],
 });
